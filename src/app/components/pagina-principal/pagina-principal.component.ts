4239892a1f621addde971ee84c298c989059eb03
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2024-06-08 12:25:26 +0200

    cambios formulario registro

--- src/app/components/pagina-principal/pagina-principal.component.ts
+++ src/app/components/pagina-principal/pagina-principal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RegistroComponent } from "../registro/registro.component";
 
 @Component({
-  selector: 'paginaPricnipal',
-  standalone: true,
-  imports: [],
-  templateUrl: './pagina-principal.component.html',
-  styleUrl: './pagina-principal.component.css'
+    selector: 'paginaPricnipal',
+    standalone: true,
+    templateUrl: './pagina-principal.component.html',
+    styleUrl: './pagina-principal.component.css',
+    imports: [RegistroComponent]
 })
 export class PaginaPrincipalComponent {
 
---
+++ src/app/components/registro/registro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'registro',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro.component.html',
+  styleUrl: './registro.component.css'
+})
+export class RegistroComponent {
+
+}
